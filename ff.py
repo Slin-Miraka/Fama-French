91a0073d3079edde60dff2a280d395affb0073ac
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-04-27 14:31:11 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -160,7 +160,7 @@ for j in range(len(ASSETS),0,-1):
 #index = ASSETS.index(ticker)
 st.write("")
 st.write("")
-st.write("**check out this for the codes in this app** [link](https://github.com/Slin-Miraka/Fama-French/edit/main/ff.py)")
+st.write("**check out this for the codes in this app** [link](https://github.com/Slin-Miraka/Fama-French/)")
 st.write("**check out this for Mean-Variance Framework app** [link](https://share.streamlit.io/slin-miraka/efficient-frontier-app/main/MVF.py)")
 email = "slin3137@uni.sydney.edu.au"
 st.write("**My E-mail**: ", email)

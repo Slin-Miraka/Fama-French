f3593e3a3526fae898e0ec56cf984379f4cdcdbd
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-04-28 14:15:04 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -191,7 +191,7 @@ st.write("")
 st.write("")
 st.subheader("**Regression Models**")
 for j in range(len(ASSETS),0,-1):
-    st.write("**{}**".format(merge.columns[-j])+ " = " + " {:.4f} ".format(regression.alphas[-j+len(ASSETS)]) + "{0:+.4f} x **MKT**".format(regression.beta_1s[-j+len(ASSETS)])  + "  {0:+.4f} x **SMB**".format(regression.beta_2s[-j+len(ASSETS)])+ "  {0:+.4f} x **HML**".format(regression.beta_3s[-j+len(ASSETS)]) + "+ ε")
+    st.write("**{}**".format(merge.columns[-j])+ " = " + " {:.4f} ".format(regression.alphas[-j+len(ASSETS)]) + "{0:+.4f} x **(MKT-RF)**".format(regression.beta_1s[-j+len(ASSETS)])  + "  {0:+.4f} x **SMB**".format(regression.beta_2s[-j+len(ASSETS)])+ "  {0:+.4f} x **HML**".format(regression.beta_3s[-j+len(ASSETS)]) + "+ ε")
 st.write("")
 st.write("")
 st.write("")
